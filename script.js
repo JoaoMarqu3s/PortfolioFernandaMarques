de132bdabb2a97fbe0514533efc991e71755fd92
JoaoMarqu3s <163063290+JoaoMarqu3s@users.noreply.github.com>
2025-08-21 21:18:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const navLinks = document.querySelectorAll('.nav-links a');
     const sections = document.querySelectorAll('section');
     const header = document.querySelector('.header');
+    
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const navBar = document.querySelector('.nav-links');
+    const body = document.body; // Novo seletor
 
     // Função para adicionar a classe 'active' ao link de navegação
     const updateActiveLink = () => {
@@ -36,6 +40,24 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     };
+    
+    // Listener para o menu hambúrguer
+    hamburgerMenu.addEventListener('click', () => {
+        navBar.classList.toggle('nav-open');
+        hamburgerMenu.classList.toggle('open');
+        body.classList.toggle('no-scroll'); // Adiciona/remove a classe que impede a rolagem
+    });
+
+    // Listener para fechar o menu ao clicar em um link
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            if (navBar.classList.contains('nav-open')) {
+                navBar.classList.remove('nav-open');
+                hamburgerMenu.classList.remove('open');
+                body.classList.remove('no-scroll'); // Remove a classe que impede a rolagem
+            }
+        });
+    });
     
     // Listeners de Eventos
     window.addEventListener('scroll', () => {
